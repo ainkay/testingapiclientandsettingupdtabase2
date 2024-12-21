9ccc762fc1408549cf0ab6542a2358d24ed43b0e
ainkay <ain.kay@kalvium.community>
2024-12-21 13:07:02 +0530

    made changes to the index.js file

--- index.js
+++ index.js
@@ -67,6 +67,8 @@ app.delete('/books/:id', (req, res) => {
 
 // Start the server
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running at http://localhost:${PORT}/`);
 });
+
